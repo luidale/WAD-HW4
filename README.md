3fb99d3db7cdd332b1e6b59bd9db60a89cfd5be2
luidale <luidale@github.com>
2021-12-09 09:10:40 +0200

    how to create table

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # WAD-HW4
 Homework 4
+
+Requirements:
+* Crate database table as in database.js
--- database.js
+++ database.js
@@ -7,3 +7,16 @@ const pool = new Pool({
  port: "5434"
 });
 module.exports = pool;
+
+/*
+Required database table can be created:
+
+CREATE TABLE posts (
+	id			bigint NOT NULL PRIMARY KEY,
+	title		character varying,
+	body 		character varying,
+	imageurl	character varying,
+	created_at	timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	likes		integer DEFAULT 0
+)
+*/
